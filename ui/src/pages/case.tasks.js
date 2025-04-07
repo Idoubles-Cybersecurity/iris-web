3caa1f289dd7e6c3055aec621aba1769bd2fb901
AzharAli <azhar.ali@idoubles.net>
2025-04-07 19:38:24 +0500

    duplicate function entries removed

--- ui/src/pages/case.tasks.js
+++ ui/src/pages/case.tasks.js
@@ -128,24 +128,24 @@ function update_task_ext(task_id, do_close) {
 }
 
 /* Delete an event from the timeline thank to its id */ 
-function delete_task(id) {
-    do_deletion_prompt("You are about to delete task #" + id)
-    .then((doDelete) => {
-        if (doDelete) {
-            let cid = get_caseid();
-            delete_request_api(`/api/v2/cases/${cid}/tasks/${id}`)
-            .done((data, textStatus) => {
-                 if (textStatus === 'nocontent') {
-                    get_tasks();
-                    $('#modal_add_task').modal('hide');
-                    notify_success('Task deleted');
-                } else {
-                     notify_error('Error deleting task')
-                 }
-            });
-        }
-    });
-}
+// function delete_task(id) {
+//     do_deletion_prompt("You are about to delete task #" + id)
+//     .then((doDelete) => {
+//         if (doDelete) {
+//             let cid = get_caseid();
+//             delete_request_api(`/api/v2/cases/${cid}/tasks/${id}`)
+//             .done((data, textStatus) => {
+//                  if (textStatus === 'nocontent') {
+//                     get_tasks();
+//                     $('#modal_add_task').modal('hide');
+//                     notify_success('Task deleted');
+//                 } else {
+//                      notify_error('Error deleting task')
+//                  }
+//             });
+//         }
+//     });
+// }
 
 /* Edit and event from the timeline thanks to its ID */
 function edit_task(id) {
@@ -421,58 +421,58 @@ $('#collapsibleContent').find('.btn[data-dismiss="collapse-action-form"]').on('c
 });
 }
 
-function save_task() {
-  $('#submit_new_task').click();
-}
+// function save_task() {
+//   $('#submit_new_task').click();
+// }
 
-function update_task(task_id) {
-  update_task_ext(task_id, true);
-}
+// function update_task(task_id) {
+//   update_task_ext(task_id, true);
+// }
 
-function update_task_ext(task_id, do_close) {
-
-  clear_api_error();
-  if (!$('form#form_new_task').valid()) {
-    return false;
-  }
-
-  if (task_id === undefined || task_id === null) {
-    task_id = g_task_id;
-  }
-
-  var data_sent = $('#form_new_task').serializeObject();
-  data_sent['task_tags'] = $('#task_tags').val();
-
-  data_sent['task_assignees_id'] = $('#task_assignees_id').val();
-  data_sent['task_status_id'] = $('#task_status_id').val();
-  ret = get_custom_attributes_fields();
-  has_error = ret[0].length > 0;
-  attributes = ret[1];
-
-  if (has_error) { return false; }
-
-  data_sent['custom_attributes'] = attributes;
-  data_sent['task_description'] = g_task_desc_editor.getValue();
-
-  $('#update_task_btn').text('Updating..');
-
-  post_request_api('tasks/update/' + task_id, JSON.stringify(data_sent), true)
-    .done((data) => {
-      if (notify_auto_api(data)) {
-        get_tasks();
-        $('#submit_new_task').text("Saved").addClass('btn-outline-success').removeClass('btn-outline-danger').removeClass('btn-outline-warning');
-        $('#last_saved').removeClass('btn-danger').addClass('btn-success');
-        $('#last_saved > i').attr('class', "fa-solid fa-file-circle-check");
-
-        if (do_close !== undefined && do_close === true) {
-          $('#modal_add_task').modal('hide');
-        }
-      }
-    })
-    .always(() => {
-      $('#update_task_btn').text('Update');
-    });
-}
+// function update_task_ext(task_id, do_close) {
+//
+//   clear_api_error();
+//   if (!$('form#form_new_task').valid()) {
+//     return false;
+//   }
+//
+//   if (task_id === undefined || task_id === null) {
+//     task_id = g_task_id;
+//   }
+//
+//   var data_sent = $('#form_new_task').serializeObject();
+//   data_sent['task_tags'] = $('#task_tags').val();
+//
+//   data_sent['task_assignees_id'] = $('#task_assignees_id').val();
+//   data_sent['task_status_id'] = $('#task_status_id').val();
+//   ret = get_custom_attributes_fields();
+//   has_error = ret[0].length > 0;
+//   attributes = ret[1];
+//
+//   if (has_error) { return false; }
+//
+//   data_sent['custom_attributes'] = attributes;
+//   data_sent['task_description'] = g_task_desc_editor.getValue();
+//
+//   $('#update_task_btn').text('Updating..');
+//
+//   post_request_api('tasks/update/' + task_id, JSON.stringify(data_sent), true)
+//     .done((data) => {
+//       if (notify_auto_api(data)) {
+//         get_tasks();
+//         $('#submit_new_task').text("Saved").addClass('btn-outline-success').removeClass('btn-outline-danger').removeClass('btn-outline-warning');
+//         $('#last_saved').removeClass('btn-danger').addClass('btn-success');
+//         $('#last_saved > i').attr('class', "fa-solid fa-file-circle-check");
+//
+//         if (do_close !== undefined && do_close === true) {
+//           $('#modal_add_task').modal('hide');
+//         }
+//       }
+//     })
+//     .always(() => {
+//       $('#update_task_btn').text('Update');
+//     });
+// }
 
 /* Delete an event from the timeline thank to its id */
 function delete_task(id) {
